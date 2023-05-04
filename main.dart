c292a21fce75dbd1cd3c91a7a8fca16d44fed872
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-05-04 18:56:36 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:teste/home_screen.dart';
+import 'package:teste/language_selection_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Short Stories App',
+      title: 'Testelingofy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: LanguageSelectionScreen(),
     );
   }
 }
+
