ace5a28e45e0f1f913662915804a059f379480a9
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-05-04 18:56:59 -0400

    Update level_selection_screen.dart

--- level_selection_screen.dart
+++ level_selection_screen.dart
@@ -1,60 +1,64 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:teste/story_screen.dart';
+import 'story_screen.dart';
 
+class LevelSelectionScreen extends StatefulWidget {
+  final String language;
+  final List<int> levels;
 
-class LevelSelectionScreen extends StatelessWidget {
+  LevelSelectionScreen({required this.language, required this.levels});
+
+  @override
+  _LevelSelectionScreenState createState() => _LevelSelectionScreenState();
+}
+
+class _LevelSelectionScreenState extends State<LevelSelectionScreen> {
+  List<Map<String, dynamic>> stories = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadStories();
+  }
+
+  Future<void> loadStories() async {
+    String data =
+    await DefaultAssetBundle.of(context).loadString("assets/${widget.language}/stories.json");
+    List<dynamic> jsonResult = json.decode(data);
+
+    setState(() {
+      stories = jsonResult.cast<Map<String, dynamic>>();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Select Level'),
+        title: Text(widget.language),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
+      body: Center(
+        child: ListView.builder(
+          itemCount: widget.levels.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ElevatedButton(
+              child: Text("Level ${widget.levels[index]}"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
                     builder: (context) => StoryScreen(
-                      language: 'Español',
-                      level: 'Principiante',
-                    )),
-              );
-            },
-            child: Text('Principiante'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => StoryScreen(
-                      language: 'Español',
-                      level: 'Intermedio',
-                    )),
-              );
-            },
-            child: Text('Intermedio'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => StoryScreen(
-                      language: 'Español',
-                      level: 'Avanzado'
-                    )),
-              );
-            },
-            child: Text('Avanzado'),
-          ),
-        ],
+                      language: widget.language,
+                      level: widget.levels[index],
+                      stories: stories,
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
 }
-
