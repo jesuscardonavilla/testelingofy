5680b4027c0ae3deabb5ddaadd583f19cac2c2bb
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-05-04 18:57:48 -0400

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,25 +1,45 @@
 import 'package:flutter/material.dart';
-
-import 'language_slection_screen.dart';
-
+import 'package:teste/level_selection_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Short Stories'),
-    ),
-    body: ElevatedButton(
-      child: Text('Select Language'),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => LanguageSelectionScreen()),
-        );
-      },
-    ),
-  );
-}
-}
+  final String language;
 
+  HomeScreen({required this.language});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Testelingofy'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to Testelingofy!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'You have selected $language.',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              child: Text('Select a level'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LevelSelectionScreen(language: language),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
