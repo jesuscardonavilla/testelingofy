9e5764b2537cea2997da82055374e4e065f2d136
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-05-04 18:57:27 -0400

    Update language_slection_screen.dart

--- language_slection_screen.dart
+++ language_slection_screen.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
-
-import 'level_selection_screen.dart';
+import 'package:teste/home_screen.dart';
 
 class LanguageSelectionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Select Language'),
+        title: Text('Select a language'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Español'),
-          onPressed: () {
-            onPressed: () {
-              _navigateToLevelSelectionScreen(context);
-            };
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Please select a language:',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              child: Text('English'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(language: 'English'),
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Español'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(language: 'Español'),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-void _navigateToLevelSelectionScreen(BuildContext context) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => LevelSelectionScreen()),
-  );
 }
