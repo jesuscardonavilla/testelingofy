aa4a8d52b3704351cfa4adf9977c85e19b4e0e2a
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-05-04 23:03:27 -0400

    Update language_slection_screen.dart

--- language_slection_screen.dart
+++ language_slection_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:teste/home_screen.dart';
+import 'package:lingofy/level_selection_screen.dart';
 
-class LanguageSelectionScreen extends StatelessWidget {
+class LanguageSelectionScreen extends StatefulWidget {
+  const LanguageSelectionScreen({Key? key, required String language}) : super(key: key);
+
+  @override
+  _LanguageScreenState createState() => _LanguageScreenState();
+}
+
+class _LanguageScreenState extends State<LanguageSelectionScreen> {
+  String _selectedLanguage = '';
+
+  void _handleLanguageChange(String language) {
+    setState(() {
+      _selectedLanguage = language;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,33 +26,118 @@ class LanguageSelectionScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Please select a language:',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 32),
+          children: [
             ElevatedButton(
+              onPressed: () {
+                _handleLanguageChange('English');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DifficultyScreen(
+                      language: 'English',
+                    ),
+                  ),
+                );
+              },
               child: Text('English'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeScreen(language: 'English'),
-                  ),
-                );
-              },
             ),
             ElevatedButton(
-              child: Text('Español'),
               onPressed: () {
+                _handleLanguageChange('Español');
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => HomeScreen(language: 'Español'),
+                    builder: (context) => DifficultyScreen(
+                      language: 'Español',
+                    ),
                   ),
                 );
               },
+              child: Text('Español'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _handleLanguageChange('Français');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DifficultyScreen(
+                      language: 'Français',
+                    ),
+                  ),
+                );
+              },
+              child: Text('Français'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _handleLanguageChange('Deutsch');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DifficultyScreen(
+                      language: 'Deutsch',
+                    ),
+                  ),
+                );
+              },
+              child: Text('Deutsch'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _handleLanguageChange('中文');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DifficultyScreen(
+                      language: '中文',
+                    ),
+                  ),
+                );
+              },
+              child: Text('中文'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _handleLanguageChange('日本語');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DifficultyScreen(
+                      language: '日本語',
+                    ),
+                  ),
+                );
+              },
+              child: Text('日本語'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _handleLanguageChange('русский');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DifficultyScreen(
+                      language: 'русский',
+                    ),
+                  ),
+                );
+              },
+              child: Text('русский'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _handleLanguageChange('한국어');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DifficultyScreen(
+                      language: '한국어',
+                    ),
+                  ),
+                );
+              },
+              child: Text('한국어'),
             ),
           ],
         ),
@@ -45,3 +145,4 @@ class LanguageSelectionScreen extends StatelessWidget {
     );
   }
 }
+
