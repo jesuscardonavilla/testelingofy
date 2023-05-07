a6092754de4e6c13317b8f0f2a963ff87f8f671c
jesuscardonavilla <131547218+jesuscardonavilla@users.noreply.github.com>
2023-05-06 23:06:01 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'level_page.dart';
+import 'home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,61 +14,5 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Short Stories')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => LevelPage(language: 'English'),
-                ),
-              );
-              // TODO: Add code to navigate to the LevelPage for English
-            },
-            child: Text('English'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => LevelPage(language: 'Italiano'),
-                ),
-              );
-
-              // TODO: Add code to navigate to the LevelPage for Italiano
-            },
-            child: Text('Italiano'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => LevelPage(language: '中文'),
-                ),
-              );
-              // TODO: Add code to navigate to the LevelPage for 中文
-            },
-            child: Text('中文'),
-          ),
-        ],
-      ),
-    );
-      // TODO: Add your widget tree for the HomePage here
-  }
-}
 
 
